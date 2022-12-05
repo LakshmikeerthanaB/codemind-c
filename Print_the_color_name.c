8ed97af61355be1f487b01f2bdd83601b7617823
LakshmikeerthanaB <119839703+LakshmikeerthanaB@users.noreply.github.com>
2022-12-05 19:07:17 +0530

    #include <stdio.h> int main() {      char ch;      scanf("%c",&ch);      if(ch=='r' || ch=='R')      {           printf("RED");      }      else if(ch=='g' || ch=='G')      {           printf("Green");      }      else if(ch=='y' || ch=='Y')      {           printf("Yellow");      }      else      {           printf("-1");      }      return 0; }

---
+++ Print_the_color_name.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+     char ch;
+     scanf("%c",&ch);
+     if(ch=='r' || ch=='R')
+     {
+          printf("RED");
+     }
+     else if(ch=='g' || ch=='G')
+     {
+          printf("Green");
+     }
+     else if(ch=='y' || ch=='Y')
+     {
+          printf("Yellow");
+     }
+     else
+     {
+          printf("-1");
+     }
+     return 0;
+}
