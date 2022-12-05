69159f3d6a335fb6c48d71dd2122ce9b10015353
LakshmikeerthanaB <119839703+LakshmikeerthanaB@users.noreply.github.com>
2022-12-05 20:30:42 +0530

    #include<stdio.h> void mainl(int a,int b,int c) {     if(a>b && a>c)     {         printf("%d",a);     }     else if(b>a && b>c)     {         printf("%d",b);     }     else if(c>a && c>b)     {         printf("%d",c);     } } int main() {     int a,b,c;     scanf("%d%d%d",&a,&b,&c);     mainl(a,b,c); }

--- Largest_number_among_3_numbers.c
+++ Largest_number_among_3_numbers.c
@@ -1,8 +1,6 @@
 #include<stdio.h>
-int main()
+void mainl(int a,int b,int c)
 {
-    int a,b,c;
-    scanf("%d%d%d",&a,&b,&c);
     if(a>b && a>c)
     {
         printf("%d",a);
@@ -15,4 +13,10 @@ int main()
     {
         printf("%d",c);
     }
+}
+int main()
+{
+    int a,b,c;
+    scanf("%d%d%d",&a,&b,&c);
+    mainl(a,b,c);
 }
